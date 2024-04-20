1b9c586aa31bd237b58369cf9251af1b1cbd5060
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-04-20 07:50:27 -0700

    bg color, font size
    Added bg color and changed the font size

--- MediAssist/App.js
+++ MediAssist/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> changed this just checking </Text>
+      <Text style={styles.text}> Welcome to MedAssist </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{  
+    fontSize: 35,
+    // color: 'red',
   },
 });
