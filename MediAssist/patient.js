a4b017ba1c0fdf9994765a375f752c864bd308fb
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-04-20 11:13:58 -0700

    Added the gemini to patient portal
    Added the gemini to the patient page and updated the UI to the latest version My has

--- MediAssist/patient.js
+++ MediAssist/patient.js
@@ -1,11 +1,17 @@
 // PatientScreen.js
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import GeminiChat from "./GeminiChat";
+import { StyleSheet, Text, View, Image, Button } 
+from 'react-native';
+import FlashMessage from "react-native-flash-message";
 
 const PatientScreen = () => {
     return (
         <View style={styles.container}>
+            <Image source={require('./assets/mediassist logo.png')}></Image>
             <Text>Patient Screen</Text>
+            <GeminiChat />
+            <FlashMessage position={"top"} />
         </View>
     );
 }
